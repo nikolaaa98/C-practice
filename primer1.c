059efc81e49f6d29682607bcb0e868f48dc5e05c
Nikola Miljkovic <sdnikola72@gmail.com>
2023-06-25 15:23:54 +0200

    Example
    first example

--- primer1.c
+++ primer1.c
@@ -1,6 +1,21 @@
+/* 
+    Izračunati površinu trougla upotrebom Heronovog obrasca.
+    • a = 2cm
+    • b = 4cm
+    • c = 5cm
+*/
+
 #include <stdio.h>
+#include <math.h>
 
-int main|(){
+int main(){
 
+    float a = 2;
+    float b = 4;
+    float c = 5;
+    float S = (a + b + c)/2;
+
+    float P = sqrt(S*(S-a)*(S-b)*(S-c));
+    printf("Povrsina zadatog trougla je : %f", P);
     return 0;
 }
